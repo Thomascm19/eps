a2312f5a8b20c66fe6e8e943298c7624d25fbe7c
Thomas Caycedo <thomascaycedo19@outlook.com>
2019-06-10 21:03:44 -0500

    Se agrega clase cajero, se modifica la vista de atencion

---
+++ src/proyecto02/Cajero.java
@@ -0,0 +1,14 @@
+
+package proyecto02;
+
+/**
+ *
+ * @author Newshore
+ */
+public class Cajero extends Persona{
+    
+    public Cajero(String cedula, String nombre, String direccion, String telefono){
+        super(cedula, nombre, direccion, telefono);
+    }
+    
+}
---
+++ src/proyecto02/Persona.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto02;
+
+/**
+ *
+ * @author Newshore
+ */
+public class Persona {
+
+    private String cedula;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+    public Persona(String cedula, String nombre, String direccion, String telefono) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the cedula
+     */
+    public String getCedula() {
+        return cedula;
+    }
+
+    /**
+     * @param cedula the cedula to set
+     */
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the direccion
+     */
+    public String getDireccion() {
+        return direccion;
+    }
+
+    /**
+     * @param direccion the direccion to set
+     */
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+}
