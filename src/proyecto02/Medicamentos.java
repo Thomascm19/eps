ed8a2be4847e2890defe2057f39bf2ccd0c849ef
Thomas Caycedo <thomascaycedo19@outlook.com>
2019-06-17 18:22:27 -0500

    fix pageback

---
+++ src/proyecto02/Medicamentos.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto02;
+
+/**
+ *
+ * @author Newshore
+ */
+public class Medicamentos {
+
+    private String Medicamentos;
+   
+
+    public Medicamentos(String Medicamentos) {
+        this.Medicamentos = Medicamentos;
+    }
+
+    /**
+     * @return the Medicamentos
+     */
+    public String getMedicamentos() {
+        return Medicamentos;
+    }
+
+    /**
+     * @param Medicamentos the Medicamentos to set
+     */
+    public void setMedicamentos(String Medicamentos) {
+        this.Medicamentos = Medicamentos;
+    }
+
+}
